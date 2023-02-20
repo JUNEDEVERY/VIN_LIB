3281e277c6c8e552681a41282b7175277742909c
герасимовна <@211-09>
2023-02-20 14:11:07 +0300

    Добавьте файлы проекта.

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,253 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using VIN_LIB.dll  ;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+
+        [TestMethod]
+        public void TestMethod_VinNumberLenght_TRUE()
+        {
+            // проверка на то, что наш вин == 17 символов
+            string vin = "2C4GJ453XYR693697";
+            Assert.IsTrue(vin.Length == 17);
+
+
+        }
+
+
+
+        [TestMethod]
+        public void TestMethod_CHECKVIN_TRUE()
+        {
+            // проверка на то, что наш вин проходит по регулярке 
+            string vin = "2C4GJ453XYR693697";
+            bool actual = VIN.CheckVin(vin);
+            Assert.IsTrue(actual);
+          
+
+        }
+        [TestMethod]
+        public void TestMethod_CHECKVIN_FALSE()
+        {
+            // проверка на то, что наш вин не проходит по регулярке 
+            string vin = "2C4GJ453XYR693697123"; // добавил больше 17 цифр
+            bool actual = VIN.CheckVin(vin);
+            Assert.IsFalse(actual);
+
+
+        }
+        [TestMethod]
+        public void TestMethod_CHECKVIN_TYPEOF_bool()
+        {
+            // проверка на то, что наш тип подходит к булл (если стринговый тип возвращается - то тру, иначе фалс)
+            string vin = "2C4GJ453XYR693697";
+            bool actual = VIN.CheckVin(vin);
+            Assert.IsInstanceOfType(actual, typeof(bool));
+
+
+        }
+        [TestMethod]
+        public void TestMethod_CHECKVIN_TYPEOF_int()
+        {
+            // проверка на то, что наш тип НЕ  подходит к булл (если стринговый тип возвращается - то тру, иначе фалс)
+            string vin = "2C4GJ453XYR693697";
+            bool actual = VIN.CheckVin(vin);
+            Assert.IsNotInstanceOfType(actual, typeof(int));
+
+
+        }
+
+
+        [TestMethod]
+        public void TestMethod_VINCHECKCOUNTRY_UZH_AMERIKA_TRUE()
+        {
+            // проверка на то, что вин номер относится к южной америке
+            string vin = "8C4GJ453XYR693697";
+            string actual = VIN.GetVINCountry(vin);
+            Assert.AreEqual(actual, "Южная Америка");
+
+            
+        }
+
+
+
+        [TestMethod]
+        public void TestMethod_VINCHECKCOUNTRY_SAME_TRUE()
+        {
+            // проверка на то, что две переменные не ссылаются на один и тот же метод
+ 
+            string vin = "8C4GJ453XYR693697";
+            string actual = VIN.GetVINCountry(vin);
+            Assert.AreNotSame(vin, actual);
+        }
+        [TestMethod]
+        public void TestMethod_VINCHECKCOUNTRY_UZH_AMERIKA_False()
+        {
+            // проверка на то, что вин номер не относится к южной америке
+            string vin = "";
+            string actual = VIN.GetVINCountry(vin);
+            Assert.AreNotEqual(actual, "Южная Америка");
+
+
+        }
+
+        [TestMethod]
+        public void TestMethod_VINCHECKCOUNTRY_SEVERNAYA_AMERIKA_TRUE()
+        {
+            // проверка на то, что вин номер относится к северной америке
+            string vin = "2C4GJ453XYR693697";
+            string actual = VIN.GetVINCountry(vin);
+            Assert.AreEqual(actual, "Северная Америка");
+
+
+        }
+        [TestMethod]
+        public void TestMethod_VINCHECKCOUNTRYnotVIN_TRUE()
+        {
+            // проверка на то, что несуществующий вин номер не относится к северной америке
+            string vin = "DACADADADA";
+            string actual = VIN.GetVINCountry(vin);
+            Assert.AreNotEqual(actual, "Северная Америка");
+
+
+        }
+        [TestMethod]
+        public void TestMethod_VINCHECKCOUNTRY_ASIA_TRUE()
+        {
+            // проверка на то, что вин номер относится относится к азии
+            string vin = "JC4GJ453XYR693697";
+            string actual = VIN.GetVINCountry(vin);
+            Assert.AreEqual(actual, "Азия");
+
+
+        }
+        [TestMethod]
+        public void TestMethod_VINCHECKCOUNTRY_True()
+        {
+            // проверка на то, что если мы добавим букву о - , а буква о нельзя использовать, то такой страны не будет
+            string vin = "2O4GJ453XYR693697";
+            string actual = VIN.GetVINCountry(vin);
+            Assert.AreEqual(actual, null);
+
+
+        }
+        [TestMethod]
+        public void TestMethod_VINCHECKCOUNTRY_OKEANIA_TRUE()
+        {
+            // проверка на то, что наш вин относится к континенту океании
+            string vin = "7C4GJ453XYR693697";
+            string except = VIN.GetVINCountry(vin);
+            Assert.AreEqual(except, "Океания");
+
+
+        }
+
+        [TestMethod]
+        public void TestMethod_VINCHECKCOUNTRY_OKEANIA_FALSE()
+        {
+            // проверка на то, что наш вин не относится к континенту океании
+            string vin = "7C4GJ453XYR693697";
+            string except = VIN.GetVINCountry(vin);
+            Assert.AreNotEqual(except, "Европа");
+
+
+        }
+
+
+        [TestMethod]
+        public void TestMethod_VINCHECKCOUNTRY_OKEANIA123()
+        {
+            // проверка на то, что наш вин дает тру при том, когда вин == континенту
+            string vin = "7C4GJ453XYR693697";
+            string except = VIN.GetVINCountry(vin);
+            Assert.IsTrue(except == "Океания");
+
+
+        }
+
+
+
+
+
+
+
+
+
+        // сложные
+        [TestMethod]
+        public void TestMethod_VINCOUNTRY_OKEANIA_FALSE()
+        {
+            // проверка на то, что наш вин  дает FALSE при том, когда вин != континенту
+            string vin = "qw]djaw9odhwqidhqwdhqw[d0qwhd098qw3hbdbqwdbwq9dbqw";
+            string except = VIN.GetVINCountry(vin);
+            Assert.IsNull(except);
+
+
+        }
+
+
+
+        [TestMethod]
+        public void TestMethod_VINCOUNTRY_ISNOTNULL_TRUE()
+        {
+            // проверка на то, что наш вин  не нулевой
+            string vin = "7C4GJ453XYR693697";
+            string except = VIN.GetVINCountry(vin);
+            Assert.IsNotNull(except);
+
+
+        }
+        [TestMethod]
+        public void TestMethod_IsNotNull_NothingException_TRUE()
+        {
+            // ПРОВЕРКА НА ТО, ЧТО НАШ МЕТОД ЧЕК ВИН НЕ ДАЕТ ЭКСШЕПШН ПРИ НУЛЕВОМ ВИНЕ
+            string vin = "";
+            Assert.ThrowsException<AssertFailedException>(() => Assert.ThrowsException<SystemException>(() => VIN.CheckVin(vin)));
+        }
+        [TestMethod]
+        public void TestMethod_IsNotNull_IsTrueVINnumber_NotException()
+        {
+            // ПРОВЕРКА НА ТО, ЧТО НАШ МЕТОД ЧЕК ВИН НЕ ДАЕТ ЭКСШЕПШН ПРИ НАЛИЧИИ ВИН НОМЕРА
+            string vin = "2C4GJ453XYR693697";
+            Assert.ThrowsException<AssertFailedException>(() => Assert.ThrowsException<SystemException>(() => VIN.CheckVin(vin)));
+        }
+
+        [TestMethod]
+        public void TestMethod_IsNotException_TRUE()
+        {
+            // ПРОВЕРКА НА ТО, ЧТО НАШ МЕТОД ЧЕК ВИН НЕ ДАЕТ ЭКСШЕПШН ПРИ определнии страны для текущего вин номера 
+            string vin = "2C4GJ453XYR693697";
+            Assert.ThrowsException<AssertFailedException>(() => Assert.ThrowsException<SystemException>(() => VIN.GetVINCountry(vin)));
+        }
+
+        [TestMethod]
+        public void TestMethod_IsNotException_GetVinCountry_TRUE()
+        {
+            // ПРОВЕРКА НА ТО, ЧТО НАШ МЕТОД ЧЕК ВИН НЕ ДАЕТ ЭКСШЕПШН ПРИ пустом вине определения страны
+            string vin = "";
+            Assert.ThrowsException<AssertFailedException>(() => Assert.ThrowsException<SystemException>(() => VIN.GetVINCountry(vin)));
+        }
+
+        [TestMethod]
+        public void TestMethod_IsAbsentException_CheckVin_TRUE()
+        {
+            // ПРОВЕРКА НА ТО, ЧТО НАШ МЕТОД ЧЕК ВИН НЕ ДАЕТ ЭКСШЕПШН ПРИ нереально большом вине
+            string vin = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+            Assert.ThrowsException<AssertFailedException>(() => Assert.ThrowsException<SystemException>(() => VIN.CheckVin(vin)));
+        }
+
+        [TestMethod]
+        public void TestMethod_IsAbsentException_CheckVinCountry_TRUE()
+        {
+            // ПРОВЕРКА НА ТО, ЧТО НАШ МЕТОД ЧЕК ВИН НЕ ДАЕТ ЭКСШЕПШН ПРИ нереально большой несуществущей страны для вин номера
+            string vin = "ЫФОВУФЫДЛВФЫЩДВОФЫЩШВФЫЩШВТФЫЩШВТФЫЩШТВЩШФЫВЩШФЫВЩШФЫВЫФВФЩШВРФЩШВРФЩШРВФЩРВФЩШРВЩШЦФРВЩШФРВ";
+            Assert.ThrowsException<AssertFailedException>(() => Assert.ThrowsException<SystemException>(() => VIN.GetVINCountry(vin)));
+        }
+    }
+}
---
+++ VIN_LIB/Class1.cs
@@ -0,0 +1,78 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace VIN_LIB.dll
+{
+    public  class VIN
+    {
+
+        public static Boolean CheckVin(string vin)
+        {
+
+            try
+            {
+                int count = 0;
+                if (!Regex.IsMatch(vin, "^[A-HJ-NPR-Z0-9]{17}$"))
+                {
+                    return false;
+                }
+                else
+                {
+                    if (vin.Length == 17)
+                    {
+                        count++;
+
+                    }
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+
+            }
+
+        }
+        public static String GetVINCountry(String vin)
+        {
+            if (CheckVin(vin))
+            {
+                //A BCDEH
+                if (vin[0] == 'A' || vin[0] == 'B' || vin[0] == 'C' || vin[0] == 'D' || vin[0] == 'E' || vin[0] == 'H')
+                {
+                    return "Африка";
+                }
+                // 'J', 'K', 'L', 'M', 'N', 'P', 'R'
+                if (vin[0] == 'J' || vin[0] == 'K' || vin[0] == 'L' || vin[0] == 'M' || vin[0] == 'N' || vin[0] == 'P' || vin[0] == 'R')
+                {
+                    return "Азия";
+                }
+                //'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+                if (vin[0] == 'S' || vin[0] == 'T' || vin[0] == 'U' || vin[0] == 'V' || vin[0] == 'W' || vin[0] == 'X' || vin[0] == 'Y' || vin[0] == 'Z')
+                {
+                    return "Европа";
+                }
+                if (vin[0] == '1' || vin[0] == '2' || vin[0] == '3' || vin[0] == '4' || vin[0] == '5')
+                {
+                    return "Северная Америка";
+                }
+                if (vin[0] == '6' || vin[0] == '7')
+                {
+                    return "Океания";   
+                }
+                if (vin[0] == '8' || vin[0] == '9')
+                {
+                    return "Южная Америка";
+                }
+            }
+          
+            return null;
+
+        }
+    }
+}
